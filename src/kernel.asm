cf8a8f07f51df1be84efba0376d823974092f347
Jared Moise <55561624+svveden@users.noreply.github.com>
2023-12-15 12:10:08 -0500

    epoch

---
+++ src/kernel.asm
@@ -0,0 +1,33 @@
+[BITS 32]
+
+global _start
+global problem
+
+extern kernel_main
+
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
+
+_start:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+
+    ; Enable the A20 line
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    call kernel_main
+
+    jmp $
+
+problem:
+    int 0
+
+times 512-($ - $$) db 0
